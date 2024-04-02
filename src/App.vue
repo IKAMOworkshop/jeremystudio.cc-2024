08d9df6443ddcabcd037951b752e0adaaa322efc
Jeremy Chang <changjeremy0226@gmail.com>
2024-04-02 08:07:47 -0700

    Setup temp link for easier development

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
     <Navigation />
-    <div>
-        <router-view></router-view>
+    <!-- TEMP LINK -->
+    <div class="temp-link flex flex-column">
+        <router-link class="text-light-gray caption-light" to="/stray">STRAY</router-link>
+        <router-link class="text-light-gray caption-light" to="/hyper">HYPER</router-link>
+        <router-link class="text-light-gray caption-light" to="/transit">TRANSIT</router-link>
+        <router-link class="text-light-gray caption-light" to="/arcane">ARCANE</router-link>
+        <router-link class="text-light-gray caption-light" to="/nebula">NEBULA</router-link>
+        <router-link class="text-light-gray caption-light" to="/angine">ANGINE</router-link>
     </div>
+    
+    <router-view></router-view>
 </template>
 
 <script setup>
