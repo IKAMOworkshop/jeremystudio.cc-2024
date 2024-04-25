18986119d83f19975e2b47d3ac433b6809e14252
Jeremy Chang <changjeremy0226@gmail.com>
2024-04-25 09:48:26 -0700

    Button hover antialiasing fixes

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="button-wrapper">
         <router-link id="button" class="primary-button caption-bold flex justify-center align-center gap-4" :to="buttonLink">
             {{ buttonText }}
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
