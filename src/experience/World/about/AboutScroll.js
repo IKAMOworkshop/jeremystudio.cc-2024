c810056b12a11541104bd9e28cad358060fb1ee2
Jeremy Chang <changjeremy0226@gmail.com>
2024-05-10 07:02:19 -0700

    Fixed model

--- src/experience/World/about/AboutScroll.js
+++ src/experience/World/about/AboutScroll.js
@@ -79,7 +79,6 @@ export default class AboutScroll{
         this.modelScale = .32
 
         this.roomModel.scale.set(this.modelScale, this.modelScale, this.modelScale)
-
         this.roomModel.children[0].material = this.roomBaked
 
         this.group = new THREE.Group()
